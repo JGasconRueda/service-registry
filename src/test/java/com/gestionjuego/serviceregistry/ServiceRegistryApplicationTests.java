93f0ebc0832436a949a2f0b4df4e9695097854e9
Jesus Gascon <jgasconrueda@gmail.com>
2021-12-29 21:47:03 +0100

    Initial commit

---
+++ src/test/java/com/gestionjuego/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionjuego.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
